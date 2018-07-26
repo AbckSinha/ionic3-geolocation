21586bb5fb020354ab2d96840ba418ae1804b8e9
Abhishek Sinha <sinha_ab@India.XoriantCorp.com>
2018-07-26 14:17:19 +0530

    Initial commit

--- src/pages/contact/contact.ts
+++ src/pages/contact/contact.ts
@@ -1,14 +1,25 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { AlertController } from 'ionic-angular';
 
 @Component({
   selector: 'page-contact',
   templateUrl: 'contact.html'
 })
 export class ContactPage {
+   
+  items = ['Food','Groceries','Cars','Ships','Bus','Truck','Jeep'];
+  constructor(public navCtrl: NavController, public alertCtrl: AlertController) {
 
-  constructor(public navCtrl: NavController) {
+  }
 
+  itemSelected(item) {
+      const alert = this.alertCtrl.create({
+      title: 'Selection',
+      subTitle: 'Your have selected' + ' ' + item,
+      buttons: ['OK']
+    });
+    alert.present();
   }
 
 }
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { Geolocation } from '@ionic-native/geolocation';
 
 @Component({
   selector: 'page-home',
@@ -7,8 +8,23 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+  constructor(public navCtrl: NavController, private geolocation: Geolocation) {
 
+  }
+  ngOnInit() {
+    this.geolocation.getCurrentPosition().then((resp) => {
+      console.log(resp.coords.latitude);
+      console.log(resp.coords.longitude);
+    }).catch((error) => {
+      console.log('Error getting location', error);
+    });
+
+    let watch = this.geolocation.watchPosition();
+    watch.subscribe((data) => {
+      // data can be a set of coordinates, or an error (if an error occurred).
+        console.log("latitude" + data.coords.latitude);
+        console.log("longitude" +data.coords.longitude);
+    });
   }
 
 }
